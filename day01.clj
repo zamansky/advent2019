6d6cd03f994f157bd22f1acf5ce54527640a342b
Mike Zamansky <zamansky@gmail.com>
2019-12-01 08:31:06 -0500

    added day1

---
+++ day01.clj
@@ -0,0 +1,30 @@
+(ns day01
+  (:require [clojure.string :as string]
+            [utils :as u]))
+
+
+(def test-masses [12 14 1969 100756])
+
+(def masses (->>
+             (slurp "day01.dat")
+             string/split-lines
+             (map u/parse-int)
+             ))
+
+
+(defn calc-mass-part1 [mass]
+  (- (quot mass 3) 2))
+
+(defn part1 [masses]
+  (reduce + (map calc-mass-part1 masses)))
+
+(defn calc-mass-part2 [mass cost]
+  (let [fuel (calc-mass-part1 mass)]
+    (if (<= fuel 0)
+      cost
+      (recur fuel (+ cost fuel)))))
+
+(defn part2 [masses]
+  (reduce + (map #(calc-mass-part2 % 0) masses )))
+
+(defn main [](print "HELLO"))
