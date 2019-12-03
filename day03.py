79f0661a22ed4eecc46416074cb50996359e0da9
Mike Zamansky <zamansky@gmail.com>
2019-12-03 18:40:45 -0500

    added some comments

--- day03.py
+++ day03.py
@@ -2,15 +2,25 @@
 dx = {"U":0,"D":0,"L":-1,"R":1}
 dy = {"U":1,"D":-1,"L":0,"R":0}
 
-deltas = {"U":(0,1),"D":(0,-1),"L":(-1,0),"R":(1,0)}
 
 def makemove(map,point,move,id,laststep):
+    """
+    simulate a single move like R3,L5 etc.
+    1. set the starting point
+    2. loop over the # of steps
+       a. Calculate the next point
+       b.If the point is not in our "map"
+         add it along with the steps so far.
+       c. If it is and it's not our ID, add our ID and update
+          the steps so far, otherwise disregard this point and
+          just move on to the next one 
+    """
     dir,steps = move
     for s in range(1,steps+1):
         point = (point[0]+dx[dir],point[1]+dy[dir])
         if point not in map:
             map[point] = {"steps":laststep+s,"intersect":False,"ids":set([id])}
-        elif id not in map[point]["ids"]:
+        elif id not in map[point]["ids"]: # point is present but this id isn't
             map[point] = {"steps": map[point]["steps"]+laststep+s,"intersect":True,"ids":map[point]["ids"].union([id])}
     return (map,point,laststep+s)
 
@@ -27,13 +37,21 @@ f = open("day03.dat")
 l1_raw  = f.readline()
 l2_raw = f.readline()
 
+# convert to a more useable form ("R",34) instead of "R34"
 l1 = [ (x[0],int(x[1:])) for x in l1_raw.split(',')]
 l2 = [ (x[0],int(x[1:])) for x in l2_raw.split(',')]
 
+#  build our map
 m = processline({},0,l1)
 m2 = processline(m,1,l2)
 
+
+# for part 1, pull out all the intersecting lines, calculate
+# the manhattan distance of the point to the origin
+# sort and take the smallest
 part1 = sorted([abs(x[0])+abs(x[1]) for x in m if m[x]['intersect']==True])[0]
-print(part1)
+print("Part 1: "+str(part1))
+
+# instead of distance look at least steps
 part2 = sorted([m[x]['steps'] for x in  m2 if m2[x]['intersect']==True])[0]
-print(part2)
+print("Part 2: "+str(part2))
