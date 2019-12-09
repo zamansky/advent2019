eca7b023a9fac9dacf27eeef11206318acaa7edf
Mike Zamansky <zamansky@gmail.com>
2019-12-09 18:01:00 -0500

    added biginteger to utils

--- utils.clj
+++ utils.clj
@@ -2,10 +2,10 @@
 
 (defn parse-int [s]
   "string->int"
-  (Integer. (re-find  #"\-?\d+" s )))
+  (biginteger (re-find  #"\-?\d+" s )))
 
 (defn abs [n] (max n (- n)))
 
 (defn char->int [c]
-  (Integer/parseInt (String/valueOf c)))
+  (Long/parseLong (String/valueOf c)))
 
